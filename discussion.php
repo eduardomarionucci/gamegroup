94f4bc84d23729c6057559a3df61966a8f773c8a
BrayanBRZ <brayanbarrosdm@gmail.com>
2023-08-07 13:31:47 -0300

    Versão com as discussões#

---
+++ discussion.php
@@ -0,0 +1,45 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: index.php");
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="./php/styleMain.css">
+    <title>Discussão</title>
+</head>
+<body>
+    
+<div id="discussionPanel" class="panel">
+    <?php include("./php/discussionPanel.php");?>
+</div>
+
+<script>
+    function sendComment() {
+        
+        var receiveUser = <?= json_encode($_SESSION["username"]); ?>;
+        var receiveDiscussion = <?= json_encode($_SESSION['discussionID']); ?>;
+        var receiveMessage = document.getElementById("receiveMessage").value;
+
+        $.post("./php/queryComment.php", {
+            receiveUser: receiveUser,
+            receiveDiscussion: receiveDiscussion,
+            receiveMessage: receiveMessage,
+        }, function(x) {
+            $("#discussionPanel").html(x);
+        }); // fechando o post
+    }
+</script>
+
+</body>
+</html>
---
+++ php/discussionPanel.php
@@ -0,0 +1,31 @@
+<?php
+include("connection.php");
+
+if (isset($_GET) && !empty($_GET)) {
+    $_SESSION['discussionID'] = $_GET["discussionID"];
+    $_SESSION['discussionUSER'] = $_GET["discussionUSER"];
+    $_SESSION['discussionMSG'] = $_GET["discussionMSG"];
+    $_SESSION['discussionGAME'] = $_GET["discussionGAME"];
+}
+
+echo 'Logado como ' . $_SESSION["username"] . '!</p>';
+
+echo $_SESSION['discussionUSER'] . " inciou uma discussão acerca de " . $_SESSION['discussionGAME'];
+echo '<textarea class="messageLarge" placeholder="' . $_SESSION['discussionMSG'] . '" readonly></textarea>';
+echo '<textarea id="receiveMessage" class="message" placeholder="Tema da discussão..."></textarea>';
+echo '<button type="submit" onClick="sendComment()" class="display">Comentar </button>';
+echo "Últimos comentários: ";
+
+$discussion = $_SESSION['discussionID'];
+$sql = "SELECT * FROM comments WHERE discussion = '$discussion' ORDER BY id DESC";
+$result = $con->query($sql);
+while ($linha = $result->fetch_object()) {
+
+    echo '<div class="commentBox">';
+
+    echo $linha->username . " comentou: ";
+    echo '<textarea class="message" placeholder="' . $linha->comment . '" readonly></textarea>';
+    echo '<input type="submit" class="display" value="relevante">';
+    echo '</div>';
+}
+$con->close();
