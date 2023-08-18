da845c18b2f6884980291e6a1f1be0ec2c196fa5
Eduardo Marionucci <85534294+eduardomarionucci@users.noreply.github.com>
2023-08-18 10:03:48 -0300

    Registro

--- register.php
+++ register.php
@@ -1,14 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./styles.css" rel="stylesheet">
     <title>Document</title>
 </head>
+
 <body>
-    <div>
-         
-    </div>
+    <div class="sections">
+        <div class="backgroundc">
+            <div class="containerbg">
+                <div class="area">
+                    <div class="logo">
+                        <a href="landing.html">GAMEGROUP</a>
+                    </div>
+                </div>
+                <div class="quote">
+                    <h1 id="gradient">Seja bem vindo!</h1>
+                </div>
+
+
+            </div>
+            <div class="blank">
+                a
+            </div>
+
+        </div>
 </body>
+
 </html>
