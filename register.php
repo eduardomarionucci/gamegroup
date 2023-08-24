9d3ad83e92fd372718bd9e3f9f2724afb0d07df2
eduardomarionucci <edudiasmarionucci@hotmail.com>
2023-08-23 22:45:11 -0300

    cadastro

--- register.php
+++ register.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./styles.css" rel="stylesheet">
-    <title>Document</title>
+    <title>Cadastro</title>
 </head>
 
 <body>
     <div class="sections">
-        <div class="backgroundc">
-            <div class="containerbg">
-                <div class="area">
-                    <div class="logo">
-                        <a href="landing.html">GAMEGROUP</a>
-                    </div>
+        <div class="containerbg">
+            <div class="area">
+                <div class="logo">
+                    <a href="index.html">GAMEGROUP</a>
                 </div>
-                <div class="quote">
-                    <h1 id="gradient">Seja bem vindo!</h1>
-                </div>
-
-
             </div>
-            <div class="blank">
-                a
+            <div class="quote">
+                <h1 id="gradient">Conecte-se a maior comunidade gamer</h1>
             </div>
-
         </div>
+
+        <div class="area-registro">
+            <div class="container-registro">
+                <div class="registro">
+                    <h1>Registro</h1>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
