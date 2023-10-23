861e2e8ae37a24ee465403f9ddc516090c11db35
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-10-23 15:43:39 -0300

    Update queryComment.php

--- php/queryComment.php
+++ php/queryComment.php
@@ -7,8 +7,14 @@ $receiveUsername = $_SESSION['usernameID'];
 $receiveDiscussion = $_POST["receiveDiscussion"];
 $receiveMessage = $_POST["receiveMessage"];
 
-$sql = "INSERT INTO comments (username, comment, discussion) values ('$receiveUsername', '$receiveMessage', '$receiveDiscussion')";
-$result = $con->query($sql);
+$sql = "INSERT INTO comments (username, comment, discussion) values (?, ?, ?)";
+$stmt = $con->prepare($sql);
+
+$cleanedMessage = strip_tags($receiveMessage);
+
+$stmt->bind_param("sss", $receiveUsername, $cleanedMessage, $receiveDiscussion);
+$stmt->execute();
+$stmt->close();
 $con->close();
 
 include("discussionPanel.php");
